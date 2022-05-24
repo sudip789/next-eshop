0ce57e51dad990f598d616be89c33fdd58364250
Sudipta Sarkar <sudipta.sarkar4545@gmail.com>
2022-05-24 16:23:13 +0530

    5

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' },{ name: 'Sam sha' },{ name: 'Raj Saha' })
+    res.status(200).json([{ name: 'John Doe' },{ name: 'Sam sha' },{ name: 'Raj Saha' }])
   }
   
